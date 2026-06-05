18e720497c40b5abd5e703e022c4f10085faee0a
myosubug <seanoh.dev@gmail.com>
2026-06-04 21:16:34 -0700

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ret = []
+        lookup = {}
+
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word in lookup:
+                lookup[sorted_word].append(word)
+            else:
+                lookup[sorted_word] = [word]
+
+        for vals in lookup.values():
+            ret.append(vals)
+
+        return ret
