4abbc63049cd4e37055095a31d5a4c3229a02aa1
myosubug <seanoh.dev@gmail.com>
2026-08-16 22:15:09 -0700

    Add: perform-string-shifts - submission-3

---
+++ Data Structures & Algorithms/perform-string-shifts/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        ret = [""] * len(s)
+        total = 0
+
+        for di, mo in shift:
+            if di == 0:
+                total -= mo
+            else:
+                total += mo
+        for i, c in enumerate(s):
+            updated = i + total
+            if updated < 0:
+                updated += len(s)
+            new_loc = updated % len(s)
+            ret[new_loc] = c
+
+        return "".join(ret)
+
+        
